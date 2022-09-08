295f94ae38187c4e4d35ddf8f201d5714d6fb116
voducloi01 <ducloi1244@gmail.com>
2022-09-08 18:01:10 +0700

    update

---
+++ server/routes/auth.js
@@ -0,0 +1,116 @@
+const express = require("express");
+const router = express.Router();
+const argon2 = require("argon2");
+const jwt = require("jsonwebtoken");
+const verifyToken = require("../middleware/auth");
+
+const User = require("../models/User");
+
+// @route GET api/auth
+// @desc Check if user is logged in
+// @access Public
+router.get("/", verifyToken, async (req, res) => {
+	try {
+		const user = await User.findById(req.userId).select("-password");
+		if (!user)
+			return res
+				.status(400)
+				.json({ success: false, message: "User not found" });
+		res.json({ success: true, user });
+	} catch (error) {
+		console.log(error);
+		res.status(500).json({ success: false, message: "Internal server error" });
+	}
+});
+
+// @route POST api/auth/register
+// @desc Register user
+// @access Public
+router.post("/register", async (req, res) => {
+	const { username, password } = req.body;
+
+	// Simple validation
+	if (!username || !password)
+		return res
+			.status(400)
+			.json({ success: false, message: "Thiếu tên người dùng và mật khẩu" });
+
+	try {
+		// Check for existing user
+		const user = await User.findOne({ username });
+
+		if (user)
+			return res
+				.status(400)
+				.json({ success: false, message: "Tên người dùng đã sử dụng" });
+
+		// All good
+		const hashedPassword = await argon2.hash(password);
+		const newUser = new User({ username, password: hashedPassword });
+		await newUser.save();
+
+		// Return token
+		const accessToken = jwt.sign(
+			{ userId: newUser._id },
+			process.env.ACCESS_TOKEN_SECRET
+		);
+
+		res.json({
+			success: true,
+			message: "User created successfully",
+			accessToken,
+		});
+	} catch (error) {
+		console.log(error);
+		res.status(500).json({ success: false, message: "Internal server error" });
+	}
+});
+
+// @route POST api/auth/login
+// @desc Login user
+// @access Public
+router.post("/login", async (req, res) => {
+	const { username, password } = req.body;
+
+	// Simple validation
+	if (!username || !password)
+		return res
+			.status(400)
+			.json({ success: false, message: "Thiếu tên người dùng và mật khẩu" });
+
+	try {
+		// Check for existing user
+		const user = await User.findOne({ username });
+		if (!user)
+			return res.status(400).json({
+				success: false,
+				message: "Tên đăng nhập hoặc mật khẩu không chính xác",
+			});
+
+		// Username found
+		const passwordValid = await argon2.verify(user.password, password);
+		if (!passwordValid)
+			return res.status(400).json({
+				success: false,
+				message: "Tên đăng nhập hoặc mật khẩu không chính xác",
+			});
+
+		// All good
+		// Return token
+		const accessToken = jwt.sign(
+			{ userId: user._id },
+			process.env.ACCESS_TOKEN_SECRET
+		);
+
+		res.json({
+			success: true,
+			message: "User logged in successfully",
+			accessToken,
+		});
+	} catch (error) {
+		console.log(error);
+		res.status(500).json({ success: false, message: "Internal server error" });
+	}
+});
+
+module.exports = router;
